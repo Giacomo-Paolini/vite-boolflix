c4e636115fff94bb05baad06851695f3d4868e9d
giacomopaolini1 <giacomopaolini08@gmail.com>
2023-05-29 19:43:39 +0200

    fix

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -26,8 +26,24 @@
                     this.store.tvSeries = r.data.results;
                     console.log("TV Series", this.store.tvSeries);
                 })
+            },
+            movieLoadedStars() {
+                axios.get("https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&language=it_IT&query=" + "Fast").then(r => {
+                    this.store.movies = r.data.results;
+                    console.log("movies", this.store.movies);
+                })
+            },
+            seriesLoadedStars() {
+                axios.get("https://api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&language=it_IT&query=" + "Fast").then(r => {
+                    this.store.tvSeries = r.data.results;
+                    console.log("TV Series", this.store.tvSeries);
+                })
             }
         },
+        mounted() {
+            this.movieLoadedStars();
+            this.seriesLoadedStars();
+        }
     }
 </script>
 
@@ -39,7 +55,7 @@
                     <div class="d-flex flex-wrap justify-content-between p-2">
                         <h1>BOOLFIX</h1>
                         <div>
-                            <input @keyup.enter="this.movieLoaded(), this.seriesLoaded()" type="search" v-model="searchFilter">
+                            <input @keyup.enter="this.movieLoaded(), this.seriesLoaded()" type="search" v-model="searchFilter" placeholder="Search">
                             <button @click="this.movieLoaded(), this.seriesLoaded()">Search</button>
                         </div>
                     </div>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -34,34 +34,40 @@
             <div class="row">
                 <h2>MOVIES</h2>
                 <div class="my-card d-flex flex-column" v-for="movie in this.store.movies">
-                    <img :src="store.imageURL + movie.poster_path">
+                    <template v-if="movie.poster_path == null">
+                        <img src="https://img.freepik.com/premium-vector/glitch-background-with-tv-noise-texture-no-signal-label-attention-sign-triangular-frame_167184-570.jpg" class="placeholder" alt="">
+                    </template>
+                    <template v-else>
+                        <img :src="store.imageURL + movie.poster_path">
+                    </template>
                     <div>
                         <h2>{{ movie.title }}</h2>
                         <p>{{ movie.original_title }}</p>
                         <span class="flags" :class="getFlags(movie.original_language)"></span><br>
-                        <template v-for="star in Math.round(movie.vote_average / 2)">
-                            <i class="fa-solid fa-star my-4"></i>
-                        </template>
-                        <template v-for="star in 5 - Math.round(movie.vote_average / 2)">
-                            <i class="fa-regular fa-star my-4"></i>
-                        </template>
+                        <div>
+                            <i v-for="index in Math.round(movie.vote_average / 2)" :key="index" class="fa-solid fa-star my-4"></i>
+                            <i v-for="index in 5 - (Math.round(movie.vote_average / 2))" :key="index" class="fa-regular fa-star my-4"></i>
+                        </div>
                     </div>
                 </div>
             </div>
             <div class="row">
                 <h2>TV SERIES</h2>
                 <div class="my-card d-flex flex-column" v-for="serie in this.store.tvSeries">
-                    <img :src="store.imageURL + serie.poster_path">
+                    <template v-if="serie.poster_path == null">
+                        <img src="https://img.freepik.com/premium-vector/glitch-background-with-tv-noise-texture-no-signal-label-attention-sign-triangular-frame_167184-570.jpg" class="placeholder" alt="">
+                    </template>
+                    <template v-else>
+                        <img :src="store.imageURL + serie.poster_path">
+                    </template>
                     <div>
                         <h2>{{ serie.name }}</h2>
                         <p>{{ serie.original_name }}</p>
                         <span :class="getFlags(serie.original_language)"></span><br>
-                        <template v-for="star in Math.round(serie.vote_average / 2)">
-                            <i class="fa-solid fa-star my-4"></i>
-                        </template>
-                        <template v-for="star in 5 - Math.round(serie.vote_average / 2)">
-                            <i class="fa-regular fa-star my-4"></i>
-                        </template>
+                        <div>
+                            <i v-for="index in Math.round(serie.vote_average / 2)" :key="index" class="fa-solid fa-star my-4"></i>
+                            <i v-for="index in 5 - (Math.round(serie.vote_average / 2))" :key="index" class="fa-regular fa-star my-4"></i>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -81,5 +87,10 @@
 
 img {
     height: 360px;
+    width: 240px;
+}
+
+.placeholder {
+    object-fit: cover;
 }
 </style>
