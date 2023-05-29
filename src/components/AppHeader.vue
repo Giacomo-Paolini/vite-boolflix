2989b410cf88632820b35f6c794f30cf012b5c19
giacomopaolini1 <giacomopaolini08@gmail.com>
2023-05-29 17:31:00 +0200

    feat: flags

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,24 +1,54 @@
-<!-- <script>
+<script>
     import {store} from '../data/store'
+    import axios from 'axios'
 
     export default {
         name: "AppHeader",
-        searchText: "",
+        data() {
+            return {
+                store,
+                searchFilter: "",
+            }
+        },
+        methods: {
+            search() {
+                let indirizzo = this.store.movieApi + this.searchFilter;
+                this.movieLoaded(indirizzo)
+            },
+            movieLoaded(address) {
+                axios.get(address).then(r => {
+                    this.store.movies = r.data.results;
+                    console.log("movies", this.store.movies);
+                })
+            }
+        },
+        computed: {
+            filterMovies() {
+                    return this.store.movieApi.filter(movie => {
+                        return movie.title.toLowerCase().startsWith(this.searchFilter)
+                })
+            }
+        },
+        mounted() {
+            this.movieLoaded(this.store.movieApi)
+        }
     }
 </script>
 
 <template>
-    <div class="container-fluid">
-        <div class="container">
-            <div class="row">
-                <div class="d-flex flex-wrap justify-content-between p-2">
-                    <h1>BOOLFIX</h1>
-                    <div>
-                        <input type="text" v-model="searchText">
-                        <button @click="clickBtn">Search</button>
+    <header>
+        <div class="container-fluid">
+            <div class="container">
+                <div class="row">
+                    <div class="d-flex flex-wrap justify-content-between p-2">
+                        <h1>BOOLFIX</h1>
+                        <div>
+                            <input @keyup.enter="search" type="search" v-model="searchFilter">
+                            <button @click="search">Search</button>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</template> -->
+    </header>
+</template>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,21 +1,36 @@
-<!-- <script>
+<script>
     import {store} from '../data/store'
 
     export default {
         name: "AppMain",
         data() {
             return {
+                store,
+                allFlags: "",
             }
         },
+        methods: {
+            getFlags(OgLanguage) {
+                if (OgLanguage == "en") {
+                    OgLanguage = "gb";
+                }
+                this.allFlags = `fi fi-${OgLanguage}`;
+                return this.allFlags;
+            }
+        }
     }
 </script>
 
 <template>
     <div class="container-fluid">
-        <div class="container">
+        <div class="container mt-3">
             <div class="row">
-                <div class="my-card">
-                    <img src="this.movie" alt="">
+                <div class="my-card d-flex flex-column" v-for="movie in this.store.movies">
+                    <h2>{{ movie.title }}</h2>
+                    <p>{{ movie.original_title }}</p>
+                    <span :class="getFlags(movie.original_language)"></span>
+                    <p>{{ movie.original_language }}</p>
+                    <p>{{ movie.vote_average }}</p>
                 </div>
             </div>
         </div>
@@ -24,7 +39,11 @@
 
 <style scoped>
 .my-card {
-    width: 20rem;
-    height: 25rem;
+    width: calc(100% / 5);
 }
-</style> -->
+
+img {
+    height: 2rem;
+    width: 15%;
+}
+</style>
